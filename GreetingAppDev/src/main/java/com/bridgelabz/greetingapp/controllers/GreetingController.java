a592817bc40f0791d703178dbc924dd386852d1c
Abhishek <you@example.com>
2022-10-12 08:42:54 +0530

    Added:[Abhishek] Greeting message Hello World with user name

--- GreetingAppDev/src/main/java/com/bridgelabz/greetingapp/controllers/GreetingController.java
+++ GreetingAppDev/src/main/java/com/bridgelabz/greetingapp/controllers/GreetingController.java
@@ -1,7 +1,9 @@
 package com.bridgelabz.greetingapp.controllers;
+import com.bridgelabz.greetingapp.entity.User;
 import com.bridgelabz.greetingapp.service.IGreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,8 +12,11 @@ public class GreetingController {
     private IGreetingService greetingService;
 
     @GetMapping("/greeting")
-    public String greeting(){
-        return greetingService.getGreetingMessage();
+    public String greeting(@RequestParam String firstName, @RequestParam String lastName){
+        User user = new User();
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        return greetingService.getGreetingMessage(user);
     }
 
 }
