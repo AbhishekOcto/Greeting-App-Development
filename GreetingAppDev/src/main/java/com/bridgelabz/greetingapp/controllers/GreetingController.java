0855ef5932fee1614449503b7da4372060bb9cf0
Abhishek <you@example.com>
2022-10-12 09:34:07 +0530

    Added:[Abhishek] save the greeting message to the repository

--- GreetingAppDev/src/main/java/com/bridgelabz/greetingapp/controllers/GreetingController.java
+++ GreetingAppDev/src/main/java/com/bridgelabz/greetingapp/controllers/GreetingController.java
@@ -1,8 +1,10 @@
 package com.bridgelabz.greetingapp.controllers;
+import com.bridgelabz.greetingapp.entity.Greeting;
 import com.bridgelabz.greetingapp.entity.User;
 import com.bridgelabz.greetingapp.service.IGreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,4 +21,11 @@ public class GreetingController {
         return greetingService.getGreetingMessage(user);
     }
 
+    @PostMapping("/greeting")
+    public Greeting addGreeting(@RequestParam String firstName, @RequestParam String lastName){
+        User user = new User();
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        return greetingService.addGreetingMessage(user);
+    }
 }
