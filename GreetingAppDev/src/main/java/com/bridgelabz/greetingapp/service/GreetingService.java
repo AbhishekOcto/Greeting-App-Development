f4ad1edf204ff51542ae624eafb436ab011ae997
Abhishek <you@example.com>
2022-10-12 08:31:54 +0530

    Added:[Abhishek] Use Service layer to get simple msg Hello World

---
+++ GreetingAppDev/src/main/java/com/bridgelabz/greetingapp/service/GreetingService.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.greetingapp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService implements IGreetingService {
+    public String getGreetingMessage(){
+        return "Hello World!";
+    }
+}
